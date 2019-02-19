8d3addd9a4575e822a69b70356c3979e1063840d
Yaroslav <git@unimarijo.com>
2019-02-19 21:06:01 +0400

    WIP: Переписал получение статуса сервисов Epic Games

---
+++ app/api/utils/translations.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+def translate_service_name(service):
+    translated_services = {
+        "Website and Forums": "Веб-сайт и форумы",
+        "Game Services": "Игровые сервисы",
+        "Fortnite": "Fortnite",
+        "Login": "Сервисы аутентификации",
+        "Parties, Friends, and Messaging": "Группы, друзья, сообщения",
+        "Epic Games store": "Магазин Epic Games",
+        "Voice Chat": "Голосовой чат",
+        "Matchmaking": "Поиск матчей",
+        "Stats and Leaderboards": "Статистика и список лидеров",
+        "Store": "Внутриигровой магазин"
+    }
+
+    try:
+        return translated_services[service]
+    except:
+        return service
+
+
+def translate_service_status(status):
+    translated_statuses = {
+        "operational": "работает в штатном режиме",
+        "degraded_performance": "ухудшенная производительность"
+    }
+
+    try:
+        return translated_statuses[status]
+    except:
+        return status
--- app/telegram/commands/status.py
+++ app/telegram/commands/status.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from app import logging
-from app.remote.redis import Redis
+from app.api.parsers.status import status as parse_services_status
 import logging
-import json
-import requests
 import asyncio
 
 
@@ -12,38 +10,9 @@ def status(client, message):
     try:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        status_url = "https://status.epicgames.com/api/v2/summary.json"
 
-        f_status = asyncio.get_event_loop().run_until_complete(Redis.execute("GET", "fortnite:status"))['details']
-        if not f_status:
-            f_status = requests.get(status_url).json()
-            asyncio.get_event_loop().run_until_complete(
-                Redis.execute("SET", "fortnite:status", json.dumps(f_status), "EX", 30))
-        else:
-            f_status = json.loads(f_status)
-
-        if f_status['status']['description'] == "All Systems Operational":
-            f_status_message = "✅ Сервисы Epic Games работают в штатном режиме."
-        elif f_status['status']['description'] == "Partial System Outage":
-            f_status_message = "⚠️ Сервисы Epic Games испытывают затруднения, возможны некоторые проблемы при " \
-                               "взаимодействии с ними. Подробная информация находится " \
-                               "[здесь](https://status.epicgames.com/)."
-        elif f_status['status']['description'] == "Major Service Outage":
-            f_status_message = "❌ Сервисы Epic Games испытывают серьезные проблемы, могут возникать серьезные " \
-                               "проблемы при взаимодействии с ними. Подробная информация находится " \
-                               "[здесь](https://status.epicgames.com/)."
-        else:
-            f_status_message = "⁉️ С сервисами Epic Games происходит нечто странное, похоже на какую-то " \
-                               "нештатную ситуацию (возможно, что это всего лишь ложь). " \
-                               "Подробная информация находится [здесь](https://status.epicgames.com/)."
-        if f_status['scheduled_maintenances']:
-            f_status_message = "\n\n🕰 Запланированные технические работы:\n"
-            num = 0
-            for maintenance in f_status['scheduled_maintenances']:
-                f_status_message = "{0}\n**{1}.** {2}".format(f_status_message, num, maintenance['name'])
-                num += 1
-
-        client.send_message(message.from_user.id, f_status_message)
+        f_status_message = asyncio.get_event_loop().run_until_complete(parse_services_status())
+        client.send_message(message.from_user.id, f_status_message, disable_web_page_preview=True)
     except Exception as e:
         logging.error("Произошла ошибка при выполнении команды /status.", exc_info=True)
         return e
