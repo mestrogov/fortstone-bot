f601d78ebac5af6054047321700f0e57e143db39
Yaroslav <git@unimarijo.com>
2019-02-26 16:21:50 +0400

    Рефакторинг кода генерации изображений
    Изображений предметов из ежедневного магазина в Fortnite

--- app/api/parsers/store.py
+++ app/api/parsers/store.py
@@ -22,48 +22,73 @@ async def store():
             for item in store_json['items']:
                 with NamedTemporaryFile(suffix=".png", delete=False) as item_file:
                     logging.debug("Сохраняется фотография предмета во временный файл: {0}".format(item_file.name))
-                    # Скачиваем фотографию предмета без названия и цены
-                    logging.debug("Рекомендуемый ли предмет: {0}".format(item['featured']))
+
+                    # Сначала пытаемся скачать фотографию предмета в полный рост (featured)
                     try:
                         item_file.write(requests.get(item['item']['images']['featured']['transparent']).content)
                     except requests.exceptions.MissingSchema:
                         item_file.write(requests.get(item['item']['images']['transparent']).content)
 
-                    image = Image.new("RGBA", (256, 256), (255, 0, 0, 0))
+                    # Создаем новое пустое изображение, которое потом будем сохранять
+                    image = Image.new("RGB", (512, 512), (255, 0, 0))
 
                     # Делаем задний фон в зависимости от редкости; оригинал: https://stackoverflow.com/a/30669765
                     center_color = colors.get_frame_center_color(item['item']['rarity'])
                     corner_color = colors.get_frame_corner_color(item['item']['rarity'])
-                    for y in range(256):
-                        for x in range(256):
-                            distance_to_center = math.sqrt((x - 256 / 2) ** 2 + (y - 256 / 2) ** 2)
-                            # Make it on a scale from 0 to 1
-                            distance_to_center = float(distance_to_center) / (math.sqrt(2) * 256 / 2)
+                    for y in range(512):
+                        for x in range(512):
+                            distance_to_center = math.sqrt((x - 512 / 2) ** 2 + (y - 512 / 2) ** 2)
+                            distance_to_center = float(distance_to_center) / (math.sqrt(2) * 512 / 2)
 
-                            # Calculate r, g, and b values
                             r = corner_color[0] * distance_to_center + center_color[0] * (1 - distance_to_center)
                             g = corner_color[1] * distance_to_center + center_color[1] * (1 - distance_to_center)
                             b = corner_color[2] * distance_to_center + center_color[2] * (1 - distance_to_center)
 
-                            # Place the pixel
                             image.putpixel((x, y), (int(r), int(g), int(b)))
 
-                    # Уменьшаем фотографию, чтобы она помещалась в рамку
-                    pass
-
-                    # Вставляем фотографию предмета поверх уже готового заднего фона
+                    # Вставляем фотографию предмета по по центру на уже готовый задний фон
+                    # Оригинал: https://stackoverflow.com/a/2563883
                     item_image = Image.open(item_file.name)
+                    item_image.thumbnail((512, 512), Image.ANTIALIAS)
+
                     item_image_width, item_image_height = item_image.size
                     background_image_width, background_image_height = image.size
                     offset = ((background_image_width - item_image_width) // 2,
                               (background_image_height - item_image_height) // 2)
+
                     image.paste(item_image, offset, item_image)
 
+                    # Переменные, которые понадобятся дальше (название, цена предмета, шрифты)
+                    item_name = item['name'].upper()
+                    item_cost = item['cost']
+                    font_name_size = int(72 - 0.75 * (len(item_name)))
+                    font_name = ImageFont.truetype("assets/fonts/BurbankBigCondensed-Bold.otf", font_name_size)
+                    font_cost = ImageFont.truetype("assets/fonts/BurbankBigCondensed-Bold.otf", 42)
+
+                    # Получаем ширину, высоту названия и цены предмета
+                    draw = ImageDraw.Draw(image)
+                    item_name_width, item_image_height = draw.textsize(item_name, font=font_name)
+                    item_cost_width, item_cost_height = draw.textsize(item_cost, font=font_cost)
+
+                    # Пишем название предмета на изображении
+                    draw.text(xy=((512 - item_name_width) // 2, 512 - 110),
+                              text=item_name, fill=(255, 255, 255), font=font_name)
+
+                    # Делаем выделение текста будущей цены (закрашиваем место, где будет написана цена)
+                    draw.rectangle(((100, 200), (150, 200)), fill=(0, 0, 0))
+
+                    # Пишем цену предмета и вставляем иконку В-Баксов
+                    vbucks_image = Image.open("assets/icons/vbucks.png")
+                    vbucks_image.thumbnail((42, 42), Image.ANTIALIAS)
+
+                    image.paste(vbucks_image, ((512 - item_cost_width) // 2, 512 - 46), vbucks_image)
+                    draw.text(xy=((512 - item_cost_width) // 2, 512 - 40),
+                              text=item_cost, fil=(255, 255, 255), font=font_cost)
+
                     # Делаем рамку в зависимости от редкости
-                    image = ImageOps.expand(image, border=3, fill=colors.get_frame_border_color(item['item']['rarity']))
-                    # font = ImageFont.truetype("assets/fonts/BurbankBigCondensed-Bold.otf", 72)
-                    # draw = ImageDraw.Draw(image)
-                    # draw.text(xy=(100, 200), text=item['name'].upper(), fill="white", font=font)
+                    image = ImageOps.expand(image, border=5, fill=colors.get_frame_border_color(item['item']['rarity']))
+
+                    # Сохраняем готовое изображение во временный файл
                     image.save(item_file.name, "PNG")
     except Exception as e:
         logging.error("Произошла ошибка при выполнении команды /store.", exc_info=True)
