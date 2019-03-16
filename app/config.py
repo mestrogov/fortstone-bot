b4895654030fc2b6c5d5b07794e84bdbcaefeb28
Yaroslav Strogov <git@unimarijo.com>
2019-03-16 11:06:59 +0400

    Постинг в VK, исправления в конфигурационном файле

--- app/config.py
+++ app/config.py
@@ -9,26 +9,26 @@ import logging
 
 try:
     # Настройки приложения
-    VERSION = str(getenv("VERSION", "Unknown"))
+    VERSION = getenv("VERSION", "Unknown")
     COMMIT = str(run(["git log --pretty=format:'%h' -n 1"], shell=True, stdout=PIPE).stdout.decode("UTF-8"))
     DEVELOPER_MODE = parse_as_boolean(getenv("DEVELOPER_MODE", False))
     # Настройки Telegram клиента
-    API_ID = int(getenv("API_ID"))
-    API_HASH = str(getenv("API_HASH"))
-    BOT_TOKEN = str(getenv("BOT_TOKEN"))
-    CHANNEL_ID = str(getenv("CHANNEL_ID"))
+    API_ID = int(getenv("API_ID")) if getenv("API_ID") else None
+    API_HASH = getenv("API_HASH")
+    BOT_TOKEN = getenv("BOT_TOKEN")
+    CHANNEL_ID = int(getenv("CHANNEL_ID")) if getenv("CHANNEL_ID") else None
     # Настройки аккаунта Epic Games для использования их API
-    EPIC_GAMES_EMAIL = str(getenv("EPIC_GAMES_EMAIL"))
-    EPIC_GAMES_PASSWORD = str(getenv("EPIC_GAMES_PASSWORD"))
-    LAUNCHER_TOKEN = str(getenv("LAUNCHER_TOKEN", "MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE="))
-    FORTNITE_TOKEN = str(getenv("FORTNITE_TOKEN", "ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="))
+    EPIC_GAMES_EMAIL = getenv("EPIC_GAMES_EMAIL")
+    EPIC_GAMES_PASSWORD = getenv("EPIC_GAMES_PASSWORD")
+    LAUNCHER_TOKEN = getenv("LAUNCHER_TOKEN", "MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=")
+    FORTNITE_TOKEN = getenv("FORTNITE_TOKEN", "ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=")
     # Настройки сообщества VK
-    VK_TOKEN = str(getenv("VK_TOKEN"))
-    VK_COMMUNITY_ID = int(getenv("VK_COMMUNITY_ID"))
+    VK_TOKEN = getenv("VK_TOKEN")
+    VK_COMMUNITY_ID = int(getenv("VK_COMMUNITY_ID")) if getenv("VK_COMMUNITY_ID") else None
 
     # Настройки Redis
-    REDIS_HOST = str(getenv("REDIS_HOST", "127.0.0.1"))
-    REDIS_PORT = int(getenv("REDIS_PORT", 6379))
+    REDIS_HOST = getenv("REDIS_HOST", "127.0.0.1")
+    REDIS_PORT = getenv("REDIS_PORT", 6379)
 except:
     logging.critical("Произошла ошибка при формировании настроек в конфигурационном файле.", exc_info=True)
     exit(1)
--- app/telegram/handlers/message.py
+++ app/telegram/handlers/message.py
@@ -1,5 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from app import logging
+from app import config
+from app.vk.session import Session as vk_session
+from tempfile import NamedTemporaryFile
+import logging
+
 
 def message(client, message):
-    pass
+    logging.critical(message)
+    if message['chat']['id'] == config.CHANNEL_ID and config.VK_TOKEN and config.VK_COMMUNITY_ID:
+        vk_client = vk_session(config.VK_TOKEN, version=5.92)
+        logging.debug(f"Клиент VK: {vk_client}.")
+
+        try:
+            assert message['photo']
+
+            with NamedTemporaryFile(suffix=".png") as photo_file:
+                client.download_media(message['photo'], photo_file.name)
+
+                upload_server = vk_client.get("photos.getWallUploadServer")['response']['upload_url']
+                logging.debug(f"Сервер для загрузки фотографии: {upload_server}")
+                uploaded_photo = vk_client.post(upload_server, files={"upload_file": open(photo_file.name, "rb")})
+                logging.debug(f"Получен ответ после загрузки фотографии: {uploaded_photo}.")
+        except AssertionError:
+            pass
+
+        params = {"owner_id": config.VK_COMMUNITY_ID, "from_group": 1, "message": message['text']}
+        post = vk_client.post("wall.post", params=params)
+
+        logging.debug(f"Опубликовано сообщение из канала в сообщество VK, ответ: {post}.")
+        return post
